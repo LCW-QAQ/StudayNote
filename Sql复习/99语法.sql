8f43f6c183ab551aceb1eb62374f36bd4c1851ff
LCW <lcwliuchongwei@qq.com>
2021-11-30 14:44:09 +0800

    SpringCloud and forget

--- Sql复习/99语法.sql
+++ Sql复习/99语法.sql
@@ -1,8 +1,8 @@
---cross join 等同于笛卡尔积
+-- cross join 等同于笛卡尔积
 select * from emp cross join dept;
---natural join 自然连接
+-- natural join 自然连接
 select * from emp natural join dept;
---外连接
+-- 外连接
 select * from emp e left join dept d on e.deptno = d.deptno;
 select * from emp e right join dept d on e.deptno = d.deptno;
 select * from emp e full join dept d on e.deptno = d.deptno;
@@ -10,7 +10,7 @@ select * from emp e full join dept d on e.deptno = d.deptno;
 select * from emp e inner join dept d on e.deptno = d.deptno;
 select * from emp e join dept d using(deptno);
 
---查询雇员名字, 所在单位, 薪水等级
+-- 查询雇员名字, 所在单位, 薪水等级
 select e.ename, d.loc, sg.grade
 from emp e
          join DEPT D on D.DEPTNO = e.DEPTNO
--- Sql复习/行转列练习.sql
+++ Sql复习/行转列练习.sql
@@ -1,22 +1,54 @@
-select * from test;
+select *
+from test;
 -- 姓名      性别     年龄
 -- --------- -------- ----
 -- 张三       男        50
-select max(decode(TYPE,1,value)) 姓名,
-       max(decode(TYPE,2,value)) 性别,
-       max(decode(TYPE,3,value)) 年龄
-from test group by T_ID;
+select max(decode(TYPE, 1, value)) 姓名,
+       max(decode(TYPE, 2, value)) 性别,
+       max(decode(TYPE, 3, value)) 年龄
+from test
+group by T_ID;
 
-select * from tmp;
+-- 练习 Begin
+
+select *
+from test;
+
+select max(if(type = '1', value, 0)) as '姓名',
+       max(if(type = '2', value, 0)) as '性别',
+       max(if(type = '3', value, 0)) as '年龄'
+from test
+group by t_id;
+
+-- 练习 End
+
+select *
+from tmp;
 --           胜 负
 -- 2005-05-09 2 2
 -- 2005-05-10 1 2
 select rq,
-       count(decode(SHENGFU,'胜','胜')) 胜,
-       count(decode(SHENGFU,'负','负')) 负
-from tmp group by rq order by rq;
+       count(decode(SHENGFU, '胜', '胜')) 胜,
+       count(decode(SHENGFU, '负', '负')) 负
+from tmp
+group by rq
+order by rq;
 
-select * from STUDENT_SCORE;
+-- 练习 Begin
+-- 表有问题
+select *
+from tmp;
+
+select time,
+       count(if(type = '1', 1, 0))  as '胜',
+       count(if(type = '2', -1, 0)) as '负'
+from tmp
+group by time;
+
+-- 练习 End
+
+select *
+from STUDENT_SCORE;
 -- 姓名   语文  数学  英语
 -- 王五    89    56   89
 select name,
@@ -26,9 +58,17 @@ select name,
 from STUDENT_SCORE
 group by NAME;
 
-select ss1.NAME,ss1.SCORE 语文,0 数学,0 英语 from STUDENT_SCORE ss1 where ss1.SUBJECT = '语文' union all
-select ss2.NAME,0 语文,ss2.SCORE 数学,0 英语 from STUDENT_SCORE ss2 where ss2.SUBJECT = '数学' union all
-select ss3.NAME,0 语文,0 数学,ss3.SCORE 英语 from STUDENT_SCORE ss3 where ss3.SUBJECT = '英语';
+select ss1.NAME, ss1.SCORE 语文, 0 数学, 0 英语
+from STUDENT_SCORE ss1
+where ss1.SUBJECT = '语文'
+union all
+select ss2.NAME, 0 语文, ss2.SCORE 数学, 0 英语
+from STUDENT_SCORE ss2
+where ss2.SUBJECT = '数学'
+union all
+select ss3.NAME, 0 语文, 0 数学, ss3.SCORE 英语
+from STUDENT_SCORE ss3
+where ss3.SUBJECT = '英语';
 
 select t.NAME, sum(t.语文), sum(t.数学), sum(t.英语)
 from (select ss1.NAME, ss1.SCORE 语文, 0 数学, 0 英语
@@ -45,9 +85,9 @@ from (select ss1.NAME, ss1.SCORE 语文, 0 数学, 0 英语
 group by t.NAME;
 
 select name,
-       max(decode(SUBJECT, '语文', case when SCORE>=80 then '优秀' when SCORE>=60 then '及格' else '不及格' end)) 语文,
-       max(decode(SUBJECT, '数学', case when SCORE>=80 then '优秀' when SCORE>=60 then '及格' else '不及格' end)) 数学,
-       max(decode(SUBJECT, '英语', case when SCORE>=80 then '优秀' when SCORE>=60 then '及格' else '不及格' end)) 英语
+       max(decode(SUBJECT, '语文', case when SCORE >= 80 then '优秀' when SCORE >= 60 then '及格' else '不及格' end)) 语文,
+       max(decode(SUBJECT, '数学', case when SCORE >= 80 then '优秀' when SCORE >= 60 then '及格' else '不及格' end)) 数学,
+       max(decode(SUBJECT, '英语', case when SCORE >= 80 then '优秀' when SCORE >= 60 then '及格' else '不及格' end)) 英语
 from STUDENT_SCORE
 group by NAME;
 
@@ -86,24 +126,30 @@ insert into yjdept(deptno,dname) values(04,'国际业务部');
 --       04                          9
 -- ------------------------------------------
 
-select * from YJ01;
+select *
+from YJ01;
 select yd.DEPTNO,
-       max(decode(yj.MONTH,'一月份',YJ)) 一月份,
-       max(decode(yj.MONTH,'二月份',YJ)) 二月份,
-       max(decode(yj.MONTH,'三月份',YJ)) 三月份
-from YJDEPT yd join YJ01 yj on yd.DEPTNO = yj.DEPTNO group by yd.DEPTNO;
+       max(decode(yj.MONTH, '一月份', YJ)) 一月份,
+       max(decode(yj.MONTH, '二月份', YJ)) 二月份,
+       max(decode(yj.MONTH, '三月份', YJ)) 三月份
+from YJDEPT yd
+         join YJ01 yj on yd.DEPTNO = yj.DEPTNO
+group by yd.DEPTNO;
 
+-- 练习 Begin
 
+select *
+from yj01;
 
+select *
+from yjdept;
 
+select yd.dname,
+       max(if(yj.month = '一月份', yj.yj, 0)) as '一月份',
+       max(if(yj.month = '二月份', yj.yj, 0)) as '二月份',
+       max(if(yj.month = '三月份', yj.yj, 0)) as '三月份'
+from yj01 yj
+         join yjdept yd on yj.deptno = yd.deptno
+group by yd.deptno;
 
-
-
-
-
-
-
-
-
-
-
+-- 练习 End
